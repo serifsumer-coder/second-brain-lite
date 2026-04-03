c7a25f27e56a2f7a647df4a606b1838c7e4efe55
Serif Sumer <serifsumer@gmail.com>
2026-04-03 17:58:32 +0300

    fix add button

--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -6,16 +6,20 @@ type Props = {
 
 export default function NoteForm({ newNote, setNewNote, addNote }: Props) {
   return (
-    <form onSubmit={addNote} className="flex gap-2 mb-6">
+    <div className="flex gap-2">
       <input
         value={newNote}
         onChange={(e) => setNewNote(e.target.value)}
-        className="border rounded-lg p-2 w-full"
         placeholder="Write a note..."
+        className="border rounded-lg p-2 w-full dark:bg-gray-800 dark:text-white"
       />
-      <button className="bg-blue-500 text-white px-4 rounded-lg">
+
+      <button
+        onClick={(e) => addNote(e as any)}
+        className="bg-blue-500 text-white px-4 rounded-lg"
+      >
         Add
       </button>
-    </form>
+    </div>
   )
 }
